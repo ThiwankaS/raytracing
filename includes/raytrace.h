d14085201a8a3eedfa15f4950dfa55081f38ea3e
Thiwanka Somachandra <thivankas@outlook.com>
2025-06-15 04:17:55 +0300

    Drawing a simple circle

--- includes/raytrace.h
+++ includes/raytrace.h
@@ -10,4 +10,34 @@
 # include "../ft_libft/get_next_line.h"
 # include "../minilibx-linux/mlx.h"
 
+# define WIDTH 800
+# define HEIGHT 600
+# define EVENT_CLOSE 17
+# define MASK_NO_EVENT 0L
+typedef struct s_mlx
+{
+	void *mlx;
+	void *win;
+	void *img;
+	char *data;
+	int  bpp;
+	int  size_line;
+	int  endian;
+} t_mlx;
+
+typedef struct s_pixel
+{
+	int	x;
+	int y;
+	int color;
+} t_pixel;
+
+typedef struct s_circle
+{
+	double x;
+	double y;
+	double r;
+	int color;
+} t_circle;
+
 #endif
